a58467aa2063a43c47c9328e60cc626435304e73
Lemon <lemonconfidence101@gmail.com>
2024-07-08 08:25:18 +0100

    fix [react router dom] error

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,17 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-router-dom']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
 })
